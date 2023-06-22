cbb14f8cc15e158170c68888a4189ec76c62cde2
qitoy <itmkan@icloud.com>
2023-06-22 21:28:54 +0900

    refactor: update deps

--- denops/camp/deps.ts
+++ denops/camp/deps.ts
@@ -1,9 +1,5 @@
 export { $ } from "https://deno.land/x/dax@0.32.0/mod.ts";
-export { open } from "https://deno.land/x/open@v0.0.6/index.ts";
-export { type Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
-export {
-  assertNumber,
-  assertString,
-} from "https://deno.land/x/unknownutil@v2.1.1/mod.ts";
-export { parse } from "https://deno.land/std@0.192.0/path/mod.ts";
+export { systemopen } from "https://deno.land/x/systemopen@v0.2.0/mod.ts";
+export { type Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+export { assert, is } from "https://deno.land/x/unknownutil@v3.0.0/mod.ts";
--- denops/camp/main.ts
+++ denops/camp/main.ts
@@ -1,17 +1,9 @@
-import {
-  $,
-  assertNumber,
-  assertString,
-  Denops,
-  fn,
-  open,
-  parse,
-} from "./deps.ts";
+import { $, assert, Denops, fn, is, systemopen } from "./deps.ts";
 
 export function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     async campNew(name: unknown): Promise<void> {
-      assertString(name);
+      assert(name, is.String);
       await competeNew(name);
       for await (const src of $.fs.expandGlob(`${name}/src/bin/*.rs`)) {
         const path = src.path;
@@ -26,7 +18,7 @@ export function main(denops: Denops): Promise<void> {
       await denops.call("camp#write", "test", output);
     },
     async campSubmit(force: unknown): Promise<void> {
-      assertNumber(force);
+      assert(force, is.Number);
       const source = await currentFullPath(denops);
       if (!force) {
         const [success, output] = await competeTest(source);
@@ -42,7 +34,7 @@ export function main(denops: Denops): Promise<void> {
         }
       }
       const uri = await competeSubmit(source);
-      await open(uri);
+      await systemopen(uri);
     },
   };
   return Promise.resolve();
@@ -57,7 +49,7 @@ async function competeNew(name: string): Promise<void> {
 }
 
 async function competeOpen(source: string): Promise<void> {
-  const { dir, name } = parse(source);
+  const { dir, name } = $.path.parse(source);
   await $`cargo compete open --bin ${name}`
     .cwd(dir)
     .noThrow()
@@ -65,7 +57,7 @@ async function competeOpen(source: string): Promise<void> {
 }
 
 async function competeTest(source: string): Promise<[boolean, string[]]> {
-  const { dir, name } = parse(source);
+  const { dir, name } = $.path.parse(source);
   const { code, combined } = await $`cargo compete test ${name}`
     .captureCombined()
     .cwd(dir)
@@ -75,7 +67,7 @@ async function competeTest(source: string): Promise<[boolean, string[]]> {
 }
 
 async function competeSubmit(source: string): Promise<string> {
-  const { dir, name } = parse(source);
+  const { dir, name } = $.path.parse(source);
   const lines = await $`cargo compete submit ${name} --no-test --no-watch`
     .captureCombined()
     .cwd(dir)
